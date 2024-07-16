434c709c16da530cb2598832919b3e8cfec72902
Constantin Voicu <voicu.constantin@outlook.com>
2024-07-16 20:14:18 +0300

    Add profile and keywords models

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+    name = models.CharField( max_length=50)
+
+    def __str__(self):
+        return self.name
+    
+class KeyWord(models.Model):
+    profile = models.ForeignKey(Profile, related_name='keywords', on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    value = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.name} ({self.grade})"
