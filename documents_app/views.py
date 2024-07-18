1101a3128990ffc86ef6412e85a96cd75c470ca7
Constantin Voicu <voicu.constantin@outlook.com>
2024-07-18 17:03:20 +0300

    Add view for scanner page

--- documents_app/views.py
+++ documents_app/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
+from profiles.models import Profile, KeyWord
 
 # Create your views here.
 
-def add_documents(request):
-    return render(request, 'documents/add_documents_page.html')
+def scanner(request):
+    job_profiles = Profile.objects.all()
+
+    if request.method == 'POST':
+        persons_count = request.POST.get('persons_count')
+        selected_job_profile = request.POST.get('job_profile')
+        documents = request.FILES.getlist('documents')
+
+    context = {
+        'job_profiles': job_profiles
+    }
+
+    return render(request, 'documents_page.html', context)
