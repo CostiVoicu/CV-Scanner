e6829459889c1a7945ea19da7550335d7f0f0847
Constantin Voicu <voicu.constantin@outlook.com>
2024-07-19 13:19:05 +0300

    Add style for edit profile page

--- profiles/forms.py
+++ profiles/forms.py
@@ -7,10 +7,17 @@ class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ['name']
+        widgets  = {
+            'name': forms.TextInput(attrs={'class': 'form-control'})
+        }
 
 class KeyWordForm(forms.ModelForm):
     class Meta:
         model = KeyWord
         fields = ['name', 'value']
+        widgets  = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'value': forms.NumberInput(attrs={'class': 'form-control'})
+        }
 
 KeyWordFormSet = inlineformset_factory(Profile, KeyWord, form=KeyWordForm, extra=1, can_delete=False)
